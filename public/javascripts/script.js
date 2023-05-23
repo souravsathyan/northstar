655d9e2e29c8d4f0d258d10e6e34833a1d45c7b2
souravsathyan <souravksathyan@gmail.com>
2023-05-23 21:24:16 +0530

    image zoom done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,35 +13,5 @@ function addToCart(prodId){
     })
 }
 
-//edit profile starts
 
-        // $('#editProfile').submit((e) => {
-        //     e.preventDefault();
-        //     var formData = new FormData($('#editProfile')[0]);
-        //     formData.append("image", $("#profilePic")[0].files[0]);
-        //     console.log(formData);
-        //     $.ajax({
-        //         url: '/postEditProfile',
-        //         method: 'post',
-        //         data: formData,
-        //         processData: false,
-        //         success: (response) => {
-        //             if (response) {
-        //                 Swal.fire({
-        //                     title: "profile edited successfully",
-        //                     text: "Your profile edited successfully.",
-        //                     icon: "success",
-        //                     confirmButton: {
-        //                         text: "OK",
-        //                         className: "btn btn-primary"
-        //                     }
-        //                 }).then(() => {
-        //                     window.location.href = "/userProfile";
-        //                 });
-        //             }
-        //         }
-        //     })
-        // });
-
-//edit profile ends
 
